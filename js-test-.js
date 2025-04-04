720199d2468938c526221e1212f9accf8fb2f602
nature-qa <nature@zsdemo2.com>
2025-04-04 15:42:22 +0530

    Update js-test-.js

--- js-test-.js
+++ js-test-.js
@@ -16,3 +16,13 @@ function changeText() {
 
 // Event listener to call changeText() when the button is clicked
 document.getElementById("myButton").addEventListener("click", changeText);
+document.getElementById("myButton").addEventListener("click", changeText);
+// script.js
+
+// Function to change the content of a webpage element
+function changeText() {
+  document.getElementById("demo").innerHTML = "Hello, World!";
+}
+
+// Event listener to call changeText() when the button is clicked
+document.getElementById("myButton").addEventListener("click", changeText);
