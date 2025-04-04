b904c7b4269c7f282cd71bfa63311ac140b99322
nature-qa <nature@zsdemo2.com>
2025-04-04 14:18:54 +0530

    Create js-test-.js

---
+++ js-test-.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to change the content of a webpage element
+function changeText() {
+  document.getElementById("demo").innerHTML = "Hello, World!";
+}
+
+// Event listener to call changeText() when the button is clicked
+document.getElementById("myButton").addEventListener("click", changeText);
