b2143b463077c9b4838501238f72363743110617
nature-qa <nature@zsdemo2.com>
2025-04-06 19:25:01 +0530

    Create pltest-test.pl

---
+++ pltest-test.pl
@@ -0,0 +1,9 @@
+# MyModule.pm
+package MyModule;
+
+# A simple subroutine
+sub hello {
+    print "Hello Nature MyModule!\n";
+}
+
+1;  # Returning true value to indicate the module was loaded successfully
