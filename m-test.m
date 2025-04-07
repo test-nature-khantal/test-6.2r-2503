042bfe25c6620dcaef58f0b73ad6a0113c798321
nature-qa <nature@zsdemo2.com>
2025-04-07 11:19:54 +0530

    Create m-test.m

---
+++ m-test.m
@@ -0,0 +1,7 @@
+% A simple MATLAB/Octave script
+x = -10:0.1:10; % Create a vector from -10 to 10
+y = sin(x);      % Compute the sine of each element
+plot(x, y);      % Plot the sine wave
+title('Nature Nature');
+xlabel('x');
+ylabel('sin(x)');
